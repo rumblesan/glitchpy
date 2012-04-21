f4e7ab283b3db8a22ec20ee79131cec21c035ebd
Guy John <guy@rumblesan.com>
2012-04-21 17:36:11 +0100

    Adding more data glitching methods
    One will move a random section of the data to another place, the other
    will reverse a random section

--- glitchr.py
+++ glitchr.py
@@ -6,7 +6,7 @@ def main():
     parser = JpegGlitcher("landscape.jpeg")
     parser.parse_file()
     parser.find_parts()
-    parser.huffman_glitch()
+    parser.data_reverse_glitch()
     parser.output_file("test.jpg")
 
 if __name__ == "__main__":
--- jpegglitcher.py
+++ jpegglitcher.py
@@ -35,11 +35,35 @@ class JpegGlitcher(JpegParser):
                 if randint(1, 100) < 60:
                     qt.data[i] = pack('B', randint(1, 254))
 
-    def data_glitch(self):
+    def data_rand_glitch(self):
         for sd in self.compressed_data:
             for i in range(sd.size - 2):
                 if randint(1, 10000) < 10:
                     if sd.data[i-1] == '\xFF':
                         sd.data[i-1] = pack('B', randint(1, 254))
                     sd.data[i] = pack('B', randint(1, 254))
+
+    def data_move_glitch(self):
+        for sd in self.compressed_data:
+            pos1 = randint(1, sd.size-2)
+            pos2 = randint(1, sd.size-2)
+            size = randint(1, int(sd.size/20))
+            if sd.data[pos1-1] == '\xFF':
+                sd.data[pos1-1] = pack('B', randint(1, 254))
+            for i in range(size):
+                sd.data[pos1+i] = sd.data[pos2+i]
+            if sd.data[pos2+size] == '\xFF':
+                sd.data[pos2+size] = pack('B', randint(1, 254))
+
+    def data_reverse_glitch(self):
+        for sd in self.compressed_data:
+            pos  = randint(1, sd.size-2)
+            size = randint(1, int(sd.size/20))
+            templist = sd.data[pos:pos+size]
+            templist.reverse()
+            for i in range(size):
+                sd.data[pos+i] = templist[i]
+            for i in range(sd.size - 2):
+                if sd.data[i] == '\xFF':
+                    sd.data[i+1] = '\x00'
 
