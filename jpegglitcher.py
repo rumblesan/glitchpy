dccbb327ba9683387ed5a70ac1cc95b90593919e
Guy John <guy@rumblesan.com>
2012-04-21 15:41:31 +0100

    Adding functional quantize_glitch method
    will randomise parts of the quantization table

--- jpegglitcher.py
+++ jpegglitcher.py
@@ -25,11 +25,14 @@ class JpegGlitcher(JpegParser):
         pass
 
     def quantize_glitch(self):
-        pass
+        for qt in self.quantizes:
+            for i in range(3, qt.size - 2): #skip first 3 bytes of data
+                if randint(1, 100) < 60:
+                    qt.data[i] = pack('B', randint(1, 254))
 
     def data_glitch(self):
         for sd in self.compressed_data:
-            for i in range(sd.size):
+            for i in range(sd.size - 2):
                 if randint(1, 10000) < 10:
                     if sd.data[i-1] == '\xFF':
                         sd.data[i-1] = pack('B', randint(1, 254))
